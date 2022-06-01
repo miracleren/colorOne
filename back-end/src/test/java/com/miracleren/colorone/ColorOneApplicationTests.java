d190e6475d1bfce9badae63c30c3d07876ccee3e
miracleren <miracleren@gmail.com>
2022-06-01 15:31:14 +0800

    init 项目

---
+++ back-end/src/test/java/com/miracleren/colorone/ColorOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracleren.colorone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
