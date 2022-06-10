3356d7a53bbc51285f76d91aebef6c7e03a108d8
miracleren <miracleren@gmail.com>
2022-06-10 11:58:16 +0800

    配置

--- back-end/color-start/src/main/java/com/colorone/ColorStartApplication.java
+++ back-end/color-start/src/main/java/com/colorone/ColorStartApplication.java
@@ -8,6 +8,7 @@ public class ColorStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ColorStartApplication.class, args);
+        System.out.println("  (♥◠‿◠)ﾉﾞ  颜一简易管理系统启动成功   ლ(´ڡ`ლ)ﾞ ");
     }
 
 }
